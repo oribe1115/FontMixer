2888776b7eecca5343b697d85643b673a1115d53
oribe <oribe.oribe1115@gmail.com>
2023-01-05 14:42:00 +0900

    saveTokenを実装

--- auth/token.go
+++ auth/token.go
@@ -35,3 +35,18 @@ func (tm *tokenManager) getTokenFromFile() (*oauth2.Token, error) {
 
 	return token, nil
 }
+
+func (tm *tokenManager) saveToken(token *oauth2.Token) error {
+	f, err := os.OpenFile(tm.filePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	err = json.NewEncoder(f).Encode(token)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
