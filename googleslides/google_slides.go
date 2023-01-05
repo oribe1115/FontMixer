aadff22dcace1be2929223089476b703d44de43e
oribe <oribe.oribe1115@gmail.com>
2023-01-05 23:10:26 +0900

    不要になった太字化のメソッドを削除

--- googleslides/google_slides.go
+++ googleslides/google_slides.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/kr/pretty"
 	"google.golang.org/api/option"
 	"google.golang.org/api/slides/v1"
 )
@@ -49,68 +48,6 @@ func (gs *GoogleSlides) RequestAlphaNumericFontUpdate() error {
 	if err != nil {
 		return err
 	}
-
-	return nil
-}
-
-// ざっくりの処理の仕方確認用
-func (gs *GoogleSlides) MakeAllTextsBold() error {
-	requests := make([]*slides.Request, 0)
-	for _, slide := range gs.presentation.Slides {
-		for _, element := range slide.PageElements {
-			shape := element.Shape
-			if shape == nil || shape.Text == nil {
-				continue
-			}
-
-			objectID := element.ObjectId
-
-			var textElement *slides.TextElement
-			for _, te := range shape.Text.TextElements {
-				if te.TextRun == nil {
-					continue
-				}
-
-				textElement = te
-			}
-
-			if textElement == nil {
-				continue
-			}
-
-			fmt.Printf("text: `%s`\n", textElement.TextRun.Content)
-
-			updateTextStyleRequest := &slides.UpdateTextStyleRequest{
-				ObjectId: objectID,
-				Style: &slides.TextStyle{
-					// TODO: 他は既存のスタイルを残すように厳密に
-					Bold: true,
-				},
-				TextRange: &slides.Range{
-					StartIndex: &textElement.StartIndex,
-					EndIndex:   &textElement.EndIndex,
-					Type:       "FIXED_RANGE",
-				},
-				Fields: "*",
-			}
-
-			request := &slides.Request{
-				UpdateTextStyle: updateTextStyleRequest,
-			}
-
-			requests = append(requests, request)
-		}
-	}
-
-	batchUpdatePresentationRequest := &slides.BatchUpdatePresentationRequest{
-		Requests: requests,
-	}
-	res, err := gs.service.Presentations.BatchUpdate(gs.presentationID, batchUpdatePresentationRequest).Do()
-	if err != nil {
-		return err
-	}
-
-	pretty.Println(res)
 
 	return nil
 }
