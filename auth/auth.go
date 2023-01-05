0b5abd3f1044dcf81d9eea7a89cde342f02e1fd6
oribe <oribe.oribe1115@gmail.com>
2023-01-05 14:55:33 +0900

    GetClientの基本構成を実装

--- auth/auth.go
+++ auth/auth.go
@@ -3,12 +3,33 @@ package auth
 import (
 	"context"
 	_ "embed"
+	"fmt"
 	"net/http"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/slides/v1"
 )
 
 //go:embed credentials.json
 var credentials []byte
 
 func GetClient(ctx context.Context) (*http.Client, error) {
-	return nil, nil
+	config, err := google.ConfigFromJSON(credentials, slides.PresentationsScope)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get config from credentials.json: %w", err)
+	}
+
+	tm := newTokenManager()
+	token, err := tm.getTokenFromFile()
+	if err != nil {
+		// TODO get token from web
+		err := tm.saveToken(token)
+		if err != nil {
+			return nil, fmt.Errorf("failed to save token: %w", err)
+		}
+	}
+
+	client := config.Client(ctx, token)
+
+	return client, nil
 }
