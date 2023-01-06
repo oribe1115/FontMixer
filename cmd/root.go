653647c9f40bc1580b037361cbfb9cdbe7256891
oribe <oribe.oribe1115@gmail.com>
2023-01-06 23:31:03 +0900

    helpでのツールの説明を書いた

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -17,17 +14,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "fontmixer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-	RunE: run,
+	Short: "Mixing fonts on Google Slides",
+	Long:  `Update fonts of alpha-numeric texts on Google Slides.`,
+	RunE:  run,
 }
 
 func run(cmd *cobra.Command, args []string) error {
